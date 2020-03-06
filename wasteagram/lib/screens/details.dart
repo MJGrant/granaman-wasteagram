d329371e97e078162b8ffb09538167b7efe1be3d
Mandi Grant <mandi018@gmail.com>
2020-03-06 10:44:17 -0600

    Centered the details column, moved children widgets into their own widgets

--- wasteagram/lib/screens/details.dart
+++ wasteagram/lib/screens/details.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Details extends StatelessWidget {
-
   static const routeName = 'Details';
   final DocumentSnapshot document;
 
@@ -12,20 +11,65 @@ class Details extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Wasteagram')),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Text(document['date']),
-            Text(document['imageURL']),
-            Text(document['quantity'].toString()),
-            Text('(' +
-                document['latitude'].toString() +
-                ', ' +
-                document['longitude'].toString() +
-                ')')
-          ],
-        ),
-      ),
+      body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              DateHeadline(date: document['date']),
+              Image(imageURL: document['imageURL']),
+              Quantity(quantity: document['quantity']),
+              Coords(lat: document['latitude'], long: document['longitude'])
+            ],
+          ),
+        );
+  }
+}
+
+class DateHeadline extends StatelessWidget {
+  final String date;
+  DateHeadline({this.date});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(date,
+          style: Theme.of(context).textTheme.headline),
     );
+  }
+}
+
+class Image extends StatelessWidget {
+  final String imageURL;
+  Image({this.imageURL});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(imageURL);
+  }
+}
+
+class Quantity extends StatelessWidget {
+  final int quantity;
+  Quantity({this.quantity});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(quantity.toString());
+  }
+}
+
+class Coords extends StatelessWidget {
+  final int lat;
+  final int long;
+
+  Coords({this.lat, this.long});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('(' +
+        lat.toString() +
+        ', ' +
+        long.toString() +
+        ')');
   }
 }
