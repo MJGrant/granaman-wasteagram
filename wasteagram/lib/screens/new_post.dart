2254ae1de4e1552985c5e048391f204be1d25a8c
Mandi Grant <mandi018@gmail.com>
2020-03-06 15:30:01 -0600

    Layout improvements to new post screen

--- wasteagram/lib/screens/new_post.dart
+++ wasteagram/lib/screens/new_post.dart
@@ -3,45 +3,119 @@ import 'package:flutter/services.dart';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'dart:math';
-
 class NewPost extends StatelessWidget {
-
   static const routeName = 'newPost';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:
-        Text('New Post')
-      ),
+      appBar: AppBar(title: Text('New Post')),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Text('Photo'),
-            Text('Number of items'),
-            TextField(
-              decoration: InputDecoration(labelText: 'Number of items'),
-              keyboardType: TextInputType.number,
-              inputFormatters: <TextInputFormatter>[WhitelistingTextInputFormatter.digitsOnly]
-            ),
-            RaisedButton(
-              onPressed: () {
-                final _random = Random();
-                Firestore.instance.collection('posts').add({
-                  'date': DateTime.now().toString(),
-                  'imageURL':'http://test.png',
-                  'latitude':12345,
-                  'longitude':98765,
-                  'quantity': _random.nextInt(100 - 0),
-                });
-                Navigator.popUntil(context, ModalRoute.withName('/'));
-              },
-              child: Icon(Icons.cloud_upload)
-            )
-          ],
-        ),
+        child: AddEntryForm(),
       ),
     );
   }
 }
+
+class PostEntryFields {
+  String quantity;
+}
+
+class AddEntryForm extends StatefulWidget {
+  @override
+  _AddEntryFormState createState() => _AddEntryFormState();
+}
+
+class _AddEntryFormState extends State<AddEntryForm> {
+  final formKey = GlobalKey<FormState>();
+
+  final postEntryFields = PostEntryFields();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(40.0, 20.0, 40.0, 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _imagePreview(),
+            _quantityFormField(),
+            SizedBox(height: 40),
+            _submitButton(),
+          ]
+        )
+      )
+    );
+  }
+
+  Widget _imagePreview() {
+    return Container(
+        width: 300,
+        height: 300,
+        color: Colors.blue,
+        child: Text('Photo')
+    );
+  }
+
+  Widget _quantityFormField() {
+    return TextFormField(
+      autofocus: true,
+      decoration:
+      InputDecoration(labelText: 'Number of items'),
+      keyboardType: TextInputType.number,
+      inputFormatters: <TextInputFormatter>[
+        WhitelistingTextInputFormatter.digitsOnly
+      ],
+      validator: (value) => validateQuantity(value),
+      onSaved: (value) {
+        postEntryFields.quantity = value;
+      }
+    );
+  }
+
+
+  Widget _submitButton() {
+    return SizedBox(
+      width: 300,
+      height: 160,
+      child: RaisedButton(
+          color: Colors.grey[300],
+          onPressed: () => validateAndSave(context),
+          child: Icon(Icons.cloud_upload)),
+    );
+  }
+
+
+  String validateQuantity(String value) {
+    if (value.isEmpty) {
+      return 'Please enter a quantity';
+    } else
+      return null;
+  }
+
+  void validateAndSave(BuildContext context) async {
+    final formState = formKey.currentState;
+
+    if (formState.validate()) {
+      formKey.currentState.save();
+
+      Firestore.instance.collection('posts').add({
+        'date': DateTime.now().toString(),
+        'imageURL': 'http://test.png',
+        'latitude': 12345,
+        'longitude': 98765,
+        'quantity': int.parse(postEntryFields.quantity),
+      });
+
+      Scaffold.of(context).showSnackBar(SnackBar(content: Text('Posting...')));
+
+      Navigator.popUntil(context, ModalRoute.withName('/'));
+    }
+  }
+}
+
+
+
