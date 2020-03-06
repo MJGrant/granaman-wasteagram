cedc134dc2025d4f066d6f891804b2992900f310
Mandi Grant <mandi018@gmail.com>
2020-03-06 12:23:44 -0600

    Photo screen now allows user to pass through to new post screen

--- wasteagram/lib/screens/photo.dart
+++ wasteagram/lib/screens/photo.dart
@@ -1,22 +1,56 @@
 import 'package:flutter/material.dart';
+import 'new_post.dart';
 
 class Photo extends StatelessWidget {
-
   static const routeName = 'Photo';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:
-      Text('Photo')
-      ),
+      appBar: AppBar(title: Text('Photo')),
       body: SingleChildScrollView(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text('Choose Photo'),
-            Text('Take Photo')
+            ChoosePhoto(),
+            TakePhoto(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ChoosePhoto extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+          child: Text('Choose Photo'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => NewPost()),
+            );
+          }
+      ),
+    );
+  }
+}
+
+class TakePhoto extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+          child: Text('Take Photo'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => NewPost()),
+            );
+          }
       ),
     );
   }
